cc53e8487a5d5435e80aa8f286abe43ea67ed8b5
Dave Simonson <dsimo@yahoo.com>
2017-12-27 20:44:53 -0600

    first part

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyBmNdJguYjrGCxH3DhyxiDIIizhMYJ1rGc';
+
+class App extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = { videos:[] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
